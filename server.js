36f7dfb00f74dd33f58d96aef399dadb81a8eed5
nikrb <nik_rb@yahoo.com>
2017-07-30 21:00:21 +0100

    fix favicon fetch error

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const moment = require( 'moment');
 
 const app = express();
 
 console.log( `using port [${process.env.PORT}] env [${process.env.NODE_ENV}]`);
 app.set('port', (process.env.PORT || 8081));
 
-app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use('/', express.static(process.cwd() + '/public'));
+
 app.use( bodyParser.json());
 
 app.get( '/', (req,res) => {
   res.sendFile( process.cwd() + '/public/index.html');
-})
+});
 
-app.get( '/:id', (req,res) => {
-  console.log( "param:", req.params);
+app.get( '/:p', (req,res) => {
+  console.log( "parm:", req.params);
+  const p = parseInt( req.params.p);
+  const ts = moment.unix( p);
+  console.log( ts);
   let stamp = null;
   let str = null;
   res.send( { unix: stamp, natural: str});
