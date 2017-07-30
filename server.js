d8f2830078ac56ebde3c9fd583812238599c44b2
nikrb <nik_rb@yahoo.com>
2017-07-30 21:33:48 +0100

    fix date parsing

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const moment = require( 'moment');
 
 const app = express();
 
-console.log( `using port [${process.env.PORT}] env [${process.env.NODE_ENV}]`);
+// console.log( `using port [${process.env.PORT}] env [${process.env.NODE_ENV}]`);
 app.set('port', (process.env.PORT || 8081));
 
-
 app.use('/', express.static(process.cwd() + '/public'));
-
-app.use( bodyParser.json());
 
 app.get( '/', (req,res) => {
   res.sendFile( process.cwd() + '/public/index.html');
 });
 
 app.get( '/:p', (req,res) => {
-  console.log( "parm:", req.params);
-  const p = parseInt( req.params.p);
-  const ts = moment.unix( p);
-  console.log( ts);
   let stamp = null;
-  let str = null;
-  res.send( { unix: stamp, natural: str});
+  let natural = null;
+  const ts = moment.unix( req.params.p);
+  if( ts.isValid()){
+    stamp = req.params.p;
+    natural = ts.format( "MMMM D, YYYY");
+  } else {
+    const nd = moment( req.params.p, "MMMM D, YYYY");
+    if( nd.isValid()){
+      stamp = nd.unix();
+      natural = nd.format( "MMMM D, YYYY");
+    }
+  }
+  res.send( { unix: stamp, natural: natural});
 });
 
 app.listen(app.get('port'), () => {
